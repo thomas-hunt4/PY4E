a21eb43b9ca066ccc1af11f9807626013c2d48d7
Thomas Hunt <thun366@wgu.edu>
2025-04-12 01:54:07 +0200

    classroom and scrap pad

---
+++ hackerrank.py
@@ -0,0 +1,21 @@
+# if __name__ == '__main__':
+
+n = int(input().strip())
+# z = int(n)
+x = bool(n % 2 == 0) 
+if x == True:
+    z = int(n)
+    and z == range(2,5):
+    print("Not Weird")
+y = n % 2 == 0
+print(x)
+print(y)
+
+
+if x == True:
+
+# if: (n % 2 == 0)
+    print('Not Weird')
+else:
+    print('Weird')    
+    
---
+++ loops.py
@@ -0,0 +1,30 @@
+
+
+bike = ['honda', 'suzuki', 'yamaha', 'kawasaki', 'ducati', 'harley']
+for bike in bike:
+    print(bike)
+
+for bike in range(3):
+    if bike == input("Enter bike name"):
+        print(bike)
+    else:
+        print("not a bike")    
+
+
+'''
+fridge = ['milk', 'eggs', 'cheese', 'butter']
+userinput = input("What we got to eat?")
+infridge = False
+for item in fridge:
+    if item == userinput:
+        infridge == True
+        break
+
+    if item == userinput:
+    print("We already have that")
+        
+    else:
+        print("add this to list")
+
+'''
+    
