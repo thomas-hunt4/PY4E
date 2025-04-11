4eb004910cea92ad8100373990e8d84dbc9d85ee
Thomas Hunt <thun366@wgu.edu>
2025-04-11 02:26:53 +0200

    added list.py and working on conditioinals

---
+++ list.py
@@ -0,0 +1,21 @@
+group5 = ['Drashti', 'Tashoy', 'Alysha', 'Thomas']
+sliced_group5 = group5[0:2]
+sliced_group5a = group5[1:3]
+
+print(sliced_group5)
+print(sliced_group5a)
+
+group5.append('Naps for days')
+
+print(group5)
+
+newmember = "Rahsaan" 
+
+if newmember in group5:
+    group5.remove
+    print("These are members of group 5:", group5)
+else:
+    group5.append(newmember)
+    print("This is our new group:", group5)
+
+
